e3700b29565a2c948e2f6ff1a72ab0b04097900a
sgtalpha <brandon.stehmeier@gmail.com>
2016-12-06 13:06:06 -0700

    Add installs for Handbreak, slack, VLC, Minecraft, LoL, Dolphin

--- installPackage.sh
+++ installPackage.sh
@@ -33,3 +33,20 @@ brew install python
 #Install Python3 via Homebrew
 brew install python3
 
+#Install Handbreak via Homebrew-Cask
+brew cask install handbreak
+
+#Install Slack via Homebrew-Cask
+brew cask install slack
+
+#Install VLC via Homebrew-Cask
+brew cask install vlc
+
+#Install Minecraft via Homebrew Cask
+brew cask install minecraft
+
+#Install LOL via Homebrew-Cask
+brew cask install league-of-legends
+
+#Install Dolphin via Homebrew-Cask
+brew cask install Dolphin
