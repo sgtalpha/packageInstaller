369f92dcf236353a8c1e00786c5ebe6c353f80e2
sgtalpha <brandon.stehmeier@gmail.com>
2016-12-06 15:47:57 -0700

    Fixed entire formatting, fixed if statement for Git

--- installPackage.sh
+++ installPackage.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
 
+##################################
+######## HOMEBREW SECTION ########
+##################################
+
 #Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-#Install Homebrew Cask extension
-brew tap caskroom/cask
+#Install Git via Homebrew if doesn't exis
+EXISTS='type git'
+
+if [ "$EXISTS" == *'not found'* ] ; then
+        brew install git
+        echo "Git successfully installed"
+else
+        echo "Git is already installed. Skipping..."
+fi
 
 #Install tmux via Homebrew
 brew install tmux
 
 #Install Nmap via Homebrew
 brew install nmap
+
+#Install Python via Homebrew
+brew install python
+
+#Install Python3 via Homebrew
+brew install python3
+
+#######################################
+######## HOMEBREW CASK SECTION ########
+#######################################
+
+#Install Homebrew-Cask extension
+brew tap caskroom/cask
 
 #Install The Unarchiver via Homebrew-Cask
 brew cask install the-unarchiver
@@ -24,12 +48,6 @@ brew cask install textmate
 #Install Vuze via Homebrew-Cask
 brew cask install vuze
 
-#Install Python via Homebrew
-brew install python
-
-#Install Python3 via Homebrew
-brew install python3
-
 #Install Handbreak via Homebrew-Cask
 brew cask install handbreak
 
@@ -39,7 +57,7 @@ brew cask install slack
 #Install VLC via Homebrew-Cask
 brew cask install vlc
 
-#Install Minecraft via Homebrew Cask
+#Install Minecraft via Homebrew-Cask
 brew cask install minecraft
 
 #Install LOL via Homebrew-Cask
@@ -47,12 +65,3 @@ brew cask install league-of-legends
 
 #Install Dolphin via Homebrew-Cask
 brew cask install Dolphin
-
-#Install Git via Homebrew if doesn't exist
-EXISTS= "type git"
-
-if [[ $EXISTS == *"not found"*]]; then
-       	brew install git
-fi
-
-echo "Installation package complete"
