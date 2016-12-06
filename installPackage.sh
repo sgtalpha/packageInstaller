c2654f67370c62b93cd97f3f8dbbc0f6a0a1dd6b
Brandon S <brandonrs@securitymetrics.com>
2016-12-06 11:19:11 -0700

    Create install; homebrew,git,tmux,nmap

---
+++ installPackage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#Install Git via Homebrew
+brew install git
+
+#Install tmux via Homebrew
+brew install tmux
+
+#install Nmap via Homebrew
+brew install nmap
