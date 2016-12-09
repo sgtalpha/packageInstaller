6d95de19906c5679c51cc953b017c85e57655888
sgtalpha <brandon.stehmeier@gmail.com>
2016-12-09 16:15:52 -0700

    Update input logic and add all outcomes and brew checks

--- inputTest.sh
+++ inputTest.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 echo "Type the FULL path to the file that lists your desired programs, then press [enter]: "
 read INPUT_FILE
@@ -10,10 +10,20 @@ for INPUT in $INPUT_FILE; do
 	MAC="$(find /Applications -iname *$INPUT* -maxdepth 1 | grep -io $INPUT | tr '[:upper:]' '[:lower:]')"
 	INPUT="$(echo $INPUT | tr '[:upper:]' '[:lower:]')"
 	
-	if test "$MAC" = "$INPUT" || test "$(echo $EXISTS | grep -io $INPUT)" = "$INPUT" ; then
+	if test "$MAC" = "$INPUT" || test "$(echo $EXISTS | grep -io $INPUT)" ; then
 		echo "$INPUT is already installed. Skipping...:"
 	else
-		echo "Installing $INPUT..."
+		if test "$(brew cask search $INPUT | grep 'No Cask*')" ; then
+			if test "$(find $(brew --prefix)/Library/Formula/$INPUT.rb 2>/dev/null)" ; then
+				echo "Installing $INPUT via Homebrew..."
+			else
+				echo "Homebrew cannot install $INPUT. Skipping..."
+			fi
+		else 
+			echo "Installing $INPUT via cask..."
+			
+		fi
+	#	echo "Installing $INPUT..."
 	fi
 done
 
