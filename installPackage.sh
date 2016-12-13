14c615d86cae420fe05b8828b414f2393c8ba48a
sgtalpha <brandon.stehmeier@gmail.com>
2016-12-13 16:39:16 -0700

    Add variables, check if brew or cask are installed, remove unnecessary comments, merge inputTest.sh logic, add for loop for programs list.

--- installPackage.sh
+++ installPackage.sh
@@ -1,74 +1,71 @@
 #!/usr/bin/env sh
 
-##################################
-######## HOMEBREW SECTION ########
-##################################
-
-#Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-#Install Git via Homebrew if doesn't exist
-INPUT="git"
- 
-EXISTS="$(which $INPUT)"
-
-if test "$(echo $EXISTS | grep -o $INPUT)" = "$INPUT" ; then
-        echo "$INPUT is already installed. Skipping..."
+#Add variables to the command / check for variables
+if test $# -lt 1; then
+	echo "You must include an input file. Use -h for help."
+	exit 0
 else
-        echo "Installing $INPUT..."
+	while test $# -gt 0; do
+		case "$1" in
+			-h|--help)
+				echo "options:"
+				echo "-h, --help		show brief help"
+				echo "-f [path], --file	[path]	provide path to input file. Note: Must be FULL path."
+				exit 0
+			;;
+			-f|--file)
+				INPUT_FILE=$(<$2)
+				shift 2
+			;;
+			*)
+				printf "Unexpected option $1.\nUse -h to see a list of accepted options.\n\n"
+				exit 0
+			;;
+		esac
+	done;
 fi
 
-#Install tmux via Homebrew
-brew install tmux
+#Install Homebrew if it doesn't exist.
 
-#Install Nmap via Homebrew
-brew install nmap
+BREW="$(which brew) | grep -o brew"
+CASK="$(ls /usr/local/Caskroom)"
 
-#Install Python via Homebrew
-brew install python
+if test "$BREW" = "brew" ; then
+	echo "Homebrew is alreaedy installed. Checking for cask..."
+else
+	echo "Installing Homebrew..."
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &>/dev/null
+fi
 
-#Install Python3 via Homebrew
-brew install python3
+#Install Homebrew Cask if it doesn't exist.
 
-#######################################
-######## HOMEBREW CASK SECTION ########
-#######################################
+if test "${#CASK}" -gt 0 ; then
+	echo "Cask is already installed. Installing your apps..."
+else
+	echo "Installing Homebrew Cask... "
+	brew tap caskroom/cask &>/dev/null
 
-#Install Homebrew-Cask extension
-brew tap caskroom/cask
+#Check each program from the input file to see if it already exists. Skip if installed already.
 
-#Install The Unarchiver via Homebrew-Cask
-brew cask install the-unarchiver
-
-#Install Spotify via Homebrew-Cask
-brew cask install spotify
-
-#Install Textmate via Homebrew-Cask
-brew cask install textmate
-
-#Install Vuze via Homebrew-Cask
-brew cask install vuze
-
-#Install Handbreak via Homebrew-Cask
-brew cask install handbreak
-
-#Install Slack via Homebrew-Cask
-brew cask install slack
-
-#Install VLC via Homebrew-Cask
-brew cask install vlc
-
-#Install Minecraft via Homebrew-Cask
-brew cask install minecraft
-
-#Install LOL via Homebrew-Cask
-brew cask install league-of-legends
-
-#Install Dolphin via Homebrew-Cask
-brew cask install Dolphin
-
-#Install Discord via Homebrew-Cask
-brew cask install discord
-
-#Install NameChanger via Homebrew-Cask
-brew cask install namechanger
+for INPUT in $INPUT_FILE; do
+	EXISTS="$(which $INPUT | tr '[:upper:]' '[:lower:]')"
+	MAC="$(find /Applications -iname *$INPUT* -maxdepth 1 | grep -io $INPUT | tr '[:upper:]' '[:lower:]')"
+	INPUT="$(echo $INPUT | tr '[:upper:]' '[:lower:]')"
+	
+	if test "$MAC" = "$INPUT" || test "$(echo $EXISTS | grep -io $INPUT)" ; then
+		echo "$INPUT is already installed. Skipping...:"
+	else
+#If it doesn't exist, see if it needs to be installed via Homebrew or Homebrew Cask; install it using appropriate method.  
+		if test "$(brew cask search $INPUT | grep 'No Cask*')" ; then
+			if test "$(find $(brew --prefix)/Library/Formula/$INPUT.rb 2>/dev/null)" ; then
+				echo "Installing $INPUT via Homebrew..."
+				brew install $INPUT
+			else
+				echo "Homebrew cannot install $INPUT. Skipping..."
+			fi
+		else 
+			echo "Installing $INPUT via cask..."
+			brew cask install $INPUT
+		fi
+	fi
+done
